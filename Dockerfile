8858de6c54e97251740cbeac1d1bf731594a2672
cavoq <Davidstromberger99@gmx.de>
2024-05-05 18:23:06 +0200

    Fix reverse SSH Tunnel

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM alpine:latest
 
 RUN apk update && \
-    apk add --no-cache openssh-client bash && \
+    apk add --no-cache openssh-client sshpass bash && \
     rm -rf /var/cache/apk/*
 
 WORKDIR /netcatcher
+
 COPY *.sh .env /netcatcher/
 
 RUN chmod +x /netcatcher/*.sh
+
+EXPOSE 22
--- netcatcher.sh
+++ netcatcher.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-source .env
-
 SERVER_ADDRESS="${SERVER_ADDRESS:-}"
 SERVER_PORT="${SERVER_PORT:-}"
 SERVER_USER="${SERVER_USER:-}"
+SERVER_PASSWORD="${SERVER_PASSWORD:-}"
 
 
 if [ -z "$SERVER_ADDRESS" ]; then
@@ -22,7 +21,10 @@ if [ -z "$SERVER_USER" ]; then
   exit 1
 fi
 
-
+if [ -z "$SERVER_PASSWORD" ]; then
+  echo "SERVER_PASSWORD is not set."
+  exit 1
+fi
 
 
 MAX_RETRIES=5
@@ -31,7 +33,7 @@ RETRY_COUNT=0
 echo "Attempting to establish reverse SSH tunnel to $SERVER_ADDRESS on port $SERVER_PORT..."
 
 while true; do
-    ssh -R "$SERVER_PORT:localhost:22" -o ServerAliveInterval=60 -i /root/.ssh/id_rsa -p "$SERVER_PORT" $SERVER_USER@"$SERVER_ADDRESS"
+    sshpass -p "$SERVER_PASSWORD" ssh -R "$SERVER_PORT:localhost:22" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -p "$SERVER_PORT" "$SERVER_USER@$SERVER_ADDRESS"
 
     if [ $? -eq 0 ]; then
         echo "Reverse SSH tunnel successfully established."
